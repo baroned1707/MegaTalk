1d81cdd511179265e7985c0706e7edad32f87d32
baroned1707 <nguyenduykhuongtqtpy@gmail.com>
2021-09-03 14:30:46 +0700

    disable ssl config

--- www/www.js
+++ www/www.js
@@ -9,18 +9,27 @@ require("dotenv").config();
 var port = process.env.PORT || 3102;
 var portSSL = process.env.PORTSSL || 3101;
 
-const cert = fs.readFileSync(path.join(__dirname, "../www") + "/cert.pem", "utf-8");
-const key = fs.readFileSync(path.join(__dirname, "../www") + "/privkey.pem", "utf-8");
-const ca = fs.readFileSync(path.join(__dirname, "../www") + "/chain.pem", "utf-8");
+// const cert = fs.readFileSync(
+//   path.join(__dirname, "../www") + "/cert.pem",
+//   "utf-8"
+// );
+// const key = fs.readFileSync(
+//   path.join(__dirname, "../www") + "/privkey.pem",
+//   "utf-8"
+// );
+// const ca = fs.readFileSync(
+//   path.join(__dirname, "../www") + "/chain.pem",
+//   "utf-8"
+// );
 
-var option = {
-  cert,
-  key,
-  ca,
-};
+// var option = {
+//   cert,
+//   key,
+//   ca,
+// };
+// const serverSSL = https.createServer(option, app);
 
 const server = http.createServer(app);
-const serverSSL = https.createServer(option, app);
 
 server.listen(port, () => {
   console.log("Server run port : " + port);
